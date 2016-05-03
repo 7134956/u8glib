d0322403ad90c063e2004bbfef95d4e14343cbc4
7134956 <7134956@gmail.com>
2016-05-03 15:56:51 +0300

    Add mode DMA full frame buffer jlx240160g-676. Code formatting.

--- csrc/u8g_dev_st75256_jlx240160g676.c
+++ csrc/u8g_dev_st75256_jlx240160g676.c
@@ -148,7 +148,7 @@ static const uint8_t u8g_dev_st75256_sleep_off[] PROGMEM = {
 };
 
 /*******************************************************************************
- *Driver for jlx240160g-676
+ * Driver for jlx240160g-676
  ******************************************************************************/
 uint8_t u8g_dev_st75256_jlx240160g676_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, void *arg) {
 	uint16_t i;
@@ -295,6 +295,52 @@ uint8_t u8g_dev_st75256_jlx240160g676_20x_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t
 	return u8g_dev_pb160v1_base_fn(u8g, dev, msg, arg);
 }
 
+/*******************************************************************************
+ * jlx240160g-676 20x DMA. Full frame buffer.
+ ******************************************************************************/
+uint8_t u8g_dev_st75256_jlx240160g676_20x_dma_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, void *arg) {
+	uint16_t i;
+	switch (msg) {
+	case U8G_DEV_MSG_INIT:
+		u8g_InitCom(u8g, dev, NULL);
+		u8g_WriteEscSeqP(u8g, dev, u8g_dev_st75256_init_seq);
+		u8g_WriteEscSeqP(u8g, dev, u8g_dev_st75256_data_start1);
+		u8g_WriteByte(u8g, dev, 1); // Set start page
+		u8g_WriteEscSeqP(u8g, dev, u8g_dev_st75256_data_start2);
+		for (i = 0; i < (WIDTH * HEIGHT/PAGE_HEIGHT); i++) {
+			u8g_WriteByte(u8g, dev, 0x00); //Заполняем дисплей белым
+		}
+		break;
+	case U8G_DEV_MSG_PAGE_NEXT: {
+		uint8_t *ptr;
+		u8g_pb_t *pb = (u8g_pb_t *) (dev->dev_mem);
+		u8g_SetChipSelect(u8g, dev, 1);
+		ptr = pb->buf;
+		u8g_WriteEscSeqP(u8g, dev, u8g_dev_st75256_data_start1);
+		u8g_WriteByte(u8g, dev, 1); // Set start page
+		u8g_WriteEscSeqP(u8g, dev, u8g_dev_st75256_data_start2);
+		u8g_WriteSequence(u8g, dev, WIDTH/PAGE_HEIGHT, ptr);
+	}
+		break;
+	case U8G_DEV_MSG_CONTRAST:
+		u8g_SetChipSelect(u8g, dev, 1);
+		u8g_SetAddress(u8g, dev, 0); /* instruction mode */
+		u8g_WriteByte(u8g, dev, 0x81);
+		u8g_SetAddress(u8g, dev, 1); /* data mode */
+		u8g_WriteByte(u8g, dev, (*(uint8_t *) arg % 64));
+		u8g_WriteByte(u8g, dev, 3 + (*(uint8_t *) arg / 64));
+		u8g_SetChipSelect(u8g, dev, 0);
+		return 1;
+	case U8G_DEV_MSG_SLEEP_ON:
+		u8g_WriteEscSeqP(u8g, dev, u8g_dev_st75256_sleep_on);
+		return 1;
+	case U8G_DEV_MSG_SLEEP_OFF:
+		u8g_WriteEscSeqP(u8g, dev, u8g_dev_st75256_sleep_off);
+		return 1;
+	}
+	return u8g_dev_pb160v1_base_fn(u8g, dev, msg, arg);
+}
+
 U8G_PB_DEV(u8g_dev_st75256_jlx240160g676_hw_spi, WIDTH, HEIGHT, PAGE_HEIGHT, u8g_dev_st75256_jlx240160g676_fn, U8G_COM_HW_SPI);
 
 uint8_t u8g_dev_st75256_jlx240160g676_4x_buf[WIDTH * 4] U8G_NOCOMMON;
@@ -304,3 +350,4 @@ u8g_dev_t u8g_dev_st75256_jlx240160g676_4x_hw_spi = { u8g_dev_st75256_jlx240160g
 uint8_t u8g_dev_st75256_jlx240160g676_20x_buf[WIDTH * HEIGHT / 8] U8G_NOCOMMON;
 u8g_pb_t u8g_dev_st75256_jlx240160g676_20x_pb = { { 160, HEIGHT, 0, 0, 0 }, WIDTH, u8g_dev_st75256_jlx240160g676_20x_buf };
 u8g_dev_t u8g_dev_st75256_jlx240160g676_20x_hw_spi = { u8g_dev_st75256_jlx240160g676_20x_fn, &u8g_dev_st75256_jlx240160g676_20x_pb, U8G_COM_HW_SPI };
+u8g_dev_t u8g_dev_st75256_jlx240160g676_20x_dma_hw_spi = { u8g_dev_st75256_jlx240160g676_20x_dma_fn, &u8g_dev_st75256_jlx240160g676_20x_pb, U8G_COM_HW_SPI };
